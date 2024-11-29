5878a00422d9bcd615d64c64306560126e6ab387
Jawpy007 <135388742+Jawpy007@users.noreply.github.com>
2024-11-29 19:07:13 +0100

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -1,6 +1,8 @@
 #Jaouen Programmation
 from Maze import *
 from math import sqrt
+from heapq import * #permet de faire des tas
+
 class A_Star:
     def __init__(self,Laby):
         self.laby = Laby
@@ -33,53 +35,71 @@ class A_Star:
 
 
     def pytha(self,cellule):
+        """ calcule de la diagonal"""
         Depart = cellule.coord()
         Arriver = self.Coord_Fin()
         return (round(sqrt((Depart[0]+Arriver[0])^2+(Depart[1]+Arriver[1])^2)*sqrt(2)*10))
 
+    def solve(self):
+        #coordonnée de debut et de fin
+        Start = self.Coord_Depart()
+        Goal = self.Coord_Fin()
 
-    def path(self):
+        print("Start:",Start,"Goal:",Goal)
 
-        Depart = self.Coord_Depart()
-        Arriver = self.Coord_Fin()
+        x,y = Start[0] , Start[1]
+
+
+
+        #creation du tas, car un tas retourne de base la plus petite valeur
+        open_set = []
+        #initialise la premiere valeur
+        heappush(open_set,(0,Start))
+
+        #Obtention du labyrinthe pour pouvoir gerer les cellules
         Laby = self.getlaby()
-        Visite = [Depart]
-        Cell_depart  = Laby.cellule(Depart[0],Depart[1])
-        Cell_arriver = Laby.cellule(Arriver[0],Arriver[1])
-        Paire = {"N":"S","S":"N","E":"O","O":"E"}
 
-        sortie = False
-        while sortie == False:
+        #dico pour savoir d'ou je viens
+        came_from = {}
 
-            CaseProche = Laby.getnear(Cell_depart)
-            #print(CaseProche)
+        #Les discrimants pour determiné qu'elle est la meilleur case
+        g_score = {Start: 0}
+        f_score = {Start: self.manhattan(Laby.cellule(x,y))}
 
-            Obti = {}
-            for case in CaseProche:
-                if None not in case:
-                    Card = case[0]
-                    x,y = case[1], case[2]
-                    Murs = Laby.cellule(x,y).getwalls()
-                    #print("je suis en ",x,y,"Mur de",Card,Murs[Card])
-                    #print(Murs[Paire[Card]] == False,(x,y) not in Visite)
-                    if Murs[Paire[Card]] == False:
-                        if (x,y) not in Visite:
-                            Discriminant = self.manhattan(Laby.cellule(x,y)) + self.pytha(Laby.cellule(x,y))
-                            Obti[(x,y)] = Discriminant
-                            if Laby.cellule(x,y).is_arriver():
-                                sortie = True
+        #set contenant les cellules deja visité, et ne pas tourné dans le vide
+        visite = set()
 
-            #print(Obti)
-            Mieux = min(Obti)
-            BestCell = Laby.cellule(Mieux[1],Mieux[0])
-            Visite.append((Mieux[1],Mieux[0]))
-            Cell_depart = BestCell
-            print(Visite)
+        #Tant qu'il reste quelque chose a regardé
+        while open_set:
+            #Coordonnée de la cellule courante
+            current = heappop(open_set)[1] #d'apres la doc : "Extraie le plus petit élément de heap en préservant l'invariant du tas" c'est donc UTILE :)
 
-        return Visite
+            #On la visite donc l'ajoute dans le set visite
+            visite.add(current)
+
+            #si chemin trouvé alors retourne la liste du chemin parcourut
+            if current == Goal:
+                #path -> liste contenant le chemin "retour" ( a l'envers donc)
+                path = []
+                #Fait le chemin "retour"
+                while current in came_from:
+                    path.append(current)
+                    current = came_from[current]
+                path.append(Start)
+
+                #c'est a l'envers :/
+                path.reverse()
+                return path
 
 
-            #print(Visite)
+            for direction in [(0,1), (1,0), (0,-1), (-1,0)]:#je regarde dans toutes les direction, deso joe pour la fonction :'(
+                neighbor = (current[0] + direction[0], current[1] + direction[1])#
+                print(neighbor)
+
+            return "Test"
+
+
+
 
 
 if __name__=="__main__":
@@ -101,6 +121,6 @@ if __name__=="__main__":
 
     # Resolution du labyrinthe
     Solver = A_Star(Laby)
-    chemin = Solver.path()
-    print("Chemin trouvé :", chemin)
+    print(Solver.solve())
+
 
