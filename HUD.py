b94b1de7bf75da8b92ba65a87b302e1e816e1f4e
isugar7 <porchiertom@gmail.com>
2024-11-30 17:37:00 +0100

    update HUD

--- HUD.py
+++ HUD.py
@@ -1,17 +1,48 @@
 import tkinter as tk
 from Maze import Maze  # Importer votre classe Maze
+from Solver import*
+import time
 
-Laby = Maze(10,10)
+if __name__=="__main__":
+    Laby = Maze(10, 10)
+
+    # Detruire des murs pour creer un chemin, destruction par paire
+    Laby.maze[0][0].destroy("E")
+    Laby.maze[1][0].destroy("O")
+
+    Laby.maze[1][0].destroy("S")
+    Laby.maze[1][1].destroy("N")
+
+    Laby.maze[1][1].destroy("E")
+    Laby.maze[2][1].destroy("O")
+
+    Laby.maze[2][1].destroy("S")
+    Laby.maze[2][2].destroy("N")
+
+    Laby.maze[0][0].depart()
+    Laby.maze[2][2].finale()
+    print(Laby)
+    solver=A_Star(Laby)
+    solution= solver.solve()
+    print(solution)
+
+
+
+
+
+
 for ligne in Laby.getmaze():
     for cellules in ligne:
         if cellules.is_depart==True:
             print(cellules)
-# Paramètres
-taille_cellule = 40  # Taille d'une cellule (en pixels)
-couleur_mur = "#444444"  # Couleur des murs
-couleur_chemin = "#DDDDDD"  # Couleur des chemins
-epaisseur_mur = 6  # Épaisseur des murs
 
+
+# Paramètres
+
+taille_cellule = 40  # Taille d'une cellule (en pixels)
+couleur_mur = "black"  # Couleur des murs
+couleur_chemin = "white"
+epaisseur_mur = 6  # Épaisseur des murs
 def dessiner_labyrinthe(canvas, maze):
     """Dessine un labyrinthe sur le canvas."""
     for ligne in range(maze.hauteur):
@@ -24,18 +55,18 @@ def dessiner_labyrinthe(canvas, maze):
             cellule = maze.cellule(ligne, colonne)
             murs = cellule.getwalls()
 
-            # Colorer le fond des chemins
-            case=canvas.create_rectangle(x1, y1, x2, y2, fill=couleur_chemin, outline=couleur_chemin)
+            # Colore le fond des chemins
+            case=canvas.create_rectangle(y1, x1, y2, x2, fill=couleur_chemin, outline=couleur_chemin)
 
-            # Dessiner les murs avec des couleurs et épaisseurs définies
+            # Dessine les murs avec des couleurs et épaisseurs définies
             if murs['N']:
-                canvas.create_line(x1, y1, x2, y1, fill=couleur_mur, width=epaisseur_mur)  # Mur nord
+                canvas.create_line(y1, x1, y2, x1, fill=couleur_mur, width=epaisseur_mur)  # Mur nord
             if murs['S']:
-                canvas.create_line(x1, y2, x2, y2, fill=couleur_mur, width=epaisseur_mur)  # Mur sud
+                canvas.create_line(y1, x2, y2, x2, fill=couleur_mur, width=epaisseur_mur)  # Mur sud
             if murs['E']:
-                canvas.create_line(x2, y1, x2, y2, fill=couleur_mur, width=epaisseur_mur)  # Mur est
+                canvas.create_line(y2, x1, y2, x2, fill=couleur_mur, width=epaisseur_mur)  # Mur est
             if murs['O']:
-                canvas.create_line(x1, y1, x1, y2, fill=couleur_mur, width=epaisseur_mur)  # Mur ouest
+                canvas.create_line(y1, x1, y1, x2, fill=couleur_mur, width=epaisseur_mur)  # Mur ouest
 
 def changer_couleur_case(canvas, ligne, colonne, couleur):
     """Change la couleur d'une case spécifique dans le labyrinthe."""
@@ -47,23 +78,19 @@ def changer_couleur_case(canvas, ligne, colonne, couleur):
     # Redessiner la case avec la nouvelle couleur
     canvas.create_rectangle(x1, y1, x2, y2, fill=couleur, outline=couleur)
 
-for ligne in Laby.maze:
-    for cellules in ligne:
-        if cellules.is_depart==True:
-            changer_couleur_case(case,cellules.coord[0],cellule.coord[1],"green")
 
-for ligne in Laby.maze:
-    for cellules in ligne:
-        if cellules.is_arriver==True:
-            changer_couleur_case(case,cellules.coord[0],cellule.coord[1],"green")
+
+
+
+
+
+
 
 
 # Initialisation de la fenêtre tkinter
+
 fenetre = tk.Tk()
 fenetre.title("Labyrinthe")
-
-# Charger le labyrinthe
-
 
 # Dimensions du canvas
 canvas_largeur = Laby.longeur * taille_cellule
@@ -76,19 +103,35 @@ canvas.pack()
 # Dessiner le labyrinthe
 dessiner_labyrinthe(canvas, Laby)
 
+
+
+for ligne in Laby.getmaze():                                     #coloriage de la cellule départ
+
+    for cellules in ligne:
+        if cellules.is_depart()==True:
+            changer_couleur_case(canvas,cellules.coord()[0],cellules.coord()[1],"green")
+        if cellules.is_arriver()==True:
+
+            print('t')
+            changer_couleur_case(canvas,cellules.coord()[0],cellules.coord()[1],"red")
+
+
+
+
+
+def trajet(index=1):                #colorie chaque le chemin
+    bouton.config(state="disabled")  # Désactive le bouton
+    global solution
+    if index < len(solution):
+        changer_couleur_case(canvas,solution[index][1],solution[index][0],"green")
+        fenetre.after(500, trajet, index + 1)
+
+bouton = tk.Button(fenetre, text="Voir le chemin", command=trajet)
+bouton.pack(pady=20)
+
+
+
+
 # Lancer l'interface graphique
 fenetre.mainloop()
 
-
-
-
-canvas_largeur = labyrinthe.longeur * taille_cellule
-canvas_hauteur = labyrinthe.hauteur * taille_cellule
-
-# Création du canvas
-canvas = tk.Canvas(fenetre, width=canvas_largeur, height=canvas_hauteur, bg="white")
-canvas.pack()
-
-# Dessiner le labyrinthe
-dessiner_labyrinthe(canvas, labyrinthe)
-fenetre.mainloop()
